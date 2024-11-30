cf9f89bd53da5a46b839c33cb3b9b17f75cf467d
oshadhalakshan <oshadhaen@gmail.com>
2024-11-30 21:22:52 +0530

    removed while loop

--- argv_1.py
+++ argv_1.py
@@ -1,9 +1,6 @@
 import sys
 
-while True:
-    try:
-        print("Hellow, My name is", sys.argv[1])
-        break
-    except IndexError:
-        print("Ex: python argv_1.py [YOUR_NAME]")
-        break
+try:
+    print("Hellow, My name is", sys.argv[1])
+except IndexError:
+    print("Ex: python argv_1.py [YOUR_NAME]")
