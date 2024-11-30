3f384424e7e4261ef833a84013866831d716f223
oshadhalakshan <oshadhaen@gmail.com>
2024-12-01 00:28:41 +0530

    file io ti api

--- api/itunes_1.py
+++ api/itunes_1.py
@@ -5,5 +5,7 @@ import sys
 if len(sys.argv) != 2:
     sys.exit
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=15&term=" + sys.argv[1])
-print(json.dumps(response.json(), indent=2))
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
+file = open(f"{sys.argv[1]}.json", "w")
+file.write(json.dumps(response.json(), indent=2))
+file.close()
