d027e375fee03bd748b6cc37eb5eae39700d77c6
oshadhalakshan <oshadhaen@gmail.com>
2024-11-30 22:44:47 +0530

    api folder

---
+++ api/itunes_2.py
@@ -0,0 +1,12 @@
+import json
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit
+
+response = requests.get("https://itunes.apple.com/search?entity=song&term=" + sys.argv[1])
+object = response.json()
+
+for result in object["results"]:
+    print(result["trackName"])
